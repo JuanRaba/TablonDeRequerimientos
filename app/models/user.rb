e03e5187c5f304ea6662e918120540287f7b2b0b
juanraba <jrabanillog@gmail.com>
2019-03-23 11:50:37 -0300

    add navbar with devise actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def show_name
+    if defined? self.name
+      self.name.present? ? self.name : self.email.split('@').first
+    else
+      self.email.split('@').first
+    end
+  end
 end
