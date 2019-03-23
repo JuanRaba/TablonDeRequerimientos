18396dc0ad9209a57fe39bb7b3fa8e3c1c7f8479
juanraba <jrabanillog@gmail.com>
2019-03-23 13:25:26 -0300

    omniauth google add mail & pass to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID   
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user| # remove or_create if want exclusive precharged accounts
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
