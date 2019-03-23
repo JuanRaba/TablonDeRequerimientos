7dbeea46ab509e44e8dfa7be552e14aeb8bb3c1a
juanraba <jrabanillog@gmail.com>
2019-03-23 13:03:53 -0300

    omniauth google avoid double render

--- app/controllers/users/omniauth_callbacks_controllers.rb
+++ app/controllers/users/omniauth_callbacks_controllers.rb
@@ -4,10 +4,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      redirect_to root_path, notice: 'Google login success'
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url, notice: 'Error al iniciar sesión con Google.'
     end
-    redirect_to root_path
   end
 end
